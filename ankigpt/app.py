63d69e257af05dd68b97fa2c400d3ba3100ca257
Shroominic <db2002dominic@gmail.com>
2024-06-15 16:32:53 -0700

    csv file responses

--- ankigpt/app.py
+++ ankigpt/app.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Form
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse, Response
 from .flashcards import generate_deck
 
 app = FastAPI()
@@ -9,8 +9,14 @@ app.get("/")(lambda: FileResponse("ankigpt/landing.html"))
 app.get("/landing_image.webp")(lambda: FileResponse("ankigpt/landing_image.webp"))
 
 
-@app.post("/generate")
-async def generate_flashcards(topic: str = Form(...)):
-    if not topic:
-        return {"error": "Topic is required"}
-    return generate_deck(topic)
+@app.get("/{topic}.csv")
+async def generate_flashcards(topic: str):
+    if topic == "":
+        raise HTTPException(status_code=400, detail="Topic is required")
+    deck = generate_deck(topic)
+    return Response(
+        content="\n".join(
+            [f"{c.question}; {c.answer}" for c in deck.flashcards],
+        ),
+        media_type="text/csv",
+    )
