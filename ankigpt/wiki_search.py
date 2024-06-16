350f20cd5a63e99d4c572c7fc9a5b44ca907235e
Julian Risch <julian.risch@deepset.ai>
2024-06-15 17:35:08 -0700

    add wiki_search given a topic

---
+++ ankigpt/wiki_search.py
@@ -0,0 +1,32 @@
+import requests
+
+
+def fetch_wiki_page(topic: str) -> str:
+    """
+    Fetch the Wikipedia page for the given topic.
+    """
+    try:
+        wiki_link = duckduckgo_search(topic)
+        if wiki_link == "":
+            return ""
+        jina_link = "https://r.jina.ai/" + wiki_link
+        response = requests.get(jina_link, timeout=30)
+        return response.text
+    except:
+        return ""
+
+
+def duckduckgo_search(query: str) -> str:
+    url = "https://api.duckduckgo.com/"
+    params = {
+        'q': "wikipedia " + query,
+        'format': 'json',
+        'pretty': 1
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        return response.json()["AbstractURL"]
+    else:
+        return ""
