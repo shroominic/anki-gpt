654586c28e5d084b5948e2d1823c9d3087fe5042
Julian Risch <julian.risch@deepset.ai>
2024-06-15 17:56:06 -0700

    filter wiki page

--- ankigpt/wiki_search.py
+++ ankigpt/wiki_search.py
@@ -1,3 +1,4 @@
+import re
 import requests
 
 
@@ -11,17 +12,21 @@ def fetch_wiki_page(topic: str) -> str:
             return ""
         jina_link = "https://r.jina.ai/" + wiki_link
         response = requests.get(jina_link, timeout=30)
-        return response.text
-    except:
+        short_response_text = response.text
+        trimmed_response = trim_after_substring(short_response_text,"References\[[edit](")
+        cleaned_string = remove_html_tags(trimmed_response)
+        return cleaned_string
+    except Exception as e:
+        print(e)
         return ""
 
 
 def duckduckgo_search(query: str) -> str:
     url = "https://api.duckduckgo.com/"
-    params = {
-        'q': "wikipedia " + query,
-        'format': 'json',
-        'pretty': 1
+    params: dict = {
+        "q": "wikipedia " + query,
+        "format": "json",
+        "pretty": 1,
     }
 
     response = requests.get(url, params=params)
@@ -30,3 +35,23 @@ def duckduckgo_search(query: str) -> str:
         return response.json()["AbstractURL"]
     else:
         return ""
+
+
+def trim_after_substring(original_string, substring):
+    # Find the position of the substring in the original string
+    pos = original_string.find(substring)
+
+    # Check if the substring was found
+    if pos != -1:
+        # Trim the string after the found position
+        return original_string[:pos]
+    else:
+        # If the substring is not found, return the original string
+        return original_string
+
+
+def remove_html_tags(text):
+    # Regular expression to match HTML tags
+    clean = re.compile(r'<.*?>')
+    # Replace matched HTML tags with an empty string
+    return re.sub(clean, '', text)
